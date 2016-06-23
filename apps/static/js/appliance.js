4a5fb24e39b861a7e25c932c2fa9aebe3384583d
“Joontae-Kim” <bexellenting@gmail.com>
2016-06-24 02:56:18 +0900

    Use electricity consumption data to calculate bills

---
+++ apps/static/js/appliance.js
@@ -0,0 +1,554 @@
+var listitem_1 = [{
+        "Id": "1000L 이상",
+        "ConsumeWatt": 69.16},
+    {
+        "Id": "900L 이상",
+        "ConsumeWatt": 50.83},
+    {
+        "Id": "800L 이상",
+        "ConsumeWatt": 49.02},
+    {
+        "Id": "700L 이상",
+        "ConsumeWatt": 44.58},
+    {
+        "Id": "600L 이상",
+        "ConsumeWatt": 43.33},
+    {
+        "Id": "500L 이상",
+        "ConsumeWatt": 41.25},
+    {
+        "Id": "400L 이상",
+        "ConsumeWatt": 37.91},
+    {
+        "Id": "300L 이상",
+        "ConsumeWatt": 32.5},
+    {
+        "Id": "200L 이상",
+        "ConsumeWatt": 40.69},
+    {
+        "Id": "100L 이상",
+        "ConsumeWatt": 29.44},
+    {
+        "Id": " 100L 미만",
+        "ConsumeWatt": 18.88}
+    ] // 냉장고
+
+var listitem_4 = [{
+        "Id": "10 인용",
+        "ConsumeWatt": 1352.8},
+    {
+        "Id": "6 인용",
+        "ConsumeWatt": 1043.3},
+    {
+        "Id": "4 인용 ",
+        "ConsumeWatt": 602.5},
+    {
+        "Id": "3 인용",
+        "ConsumeWatt": 667.9},
+    {
+        "Id": "1인용",
+        "ConsumeWatt": 230.0}
+    ] // 전기밥솥 
+
+var listitem_5 = [{
+        "Id": "800L 이상",
+        "ConsumeWatt": 39.02},
+    {
+        "Id": "500L 이상",
+        "ConsumeWatt": 27.91},
+    {
+        "Id": "400L 이상",
+        "ConsumeWatt": 24.72},
+    {
+        "Id": "300L 이상",
+        "ConsumeWatt": 22.08},
+    {
+        "Id": "200L 이상",
+        "ConsumeWatt": 18.75},
+    {
+        "Id": "100L 이상",
+        "ConsumeWatt": 18.19}
+    ] // 김치냉장고 
+
+var listitem_10 = [{
+        "Id": "전자레인지",
+        "ConsumeWatt": 1104}
+    ] // 전자레인지
+  
+var listitem_11 = [{
+        "Id": "비데",
+        "ConsumeWatt": 1000},
+    ] // 비데
+  
+var listitem_13 = [{
+        "Id": "65인치 이상",
+        "ConsumeWatt": 68},
+    {
+        "Id": "50인치 이상",
+        "ConsumeWatt": 57},
+    {
+        "Id": "42인치 이상",
+        "ConsumeWatt": 49},
+    {
+        "Id": "34인치 이상",
+        "ConsumeWatt": 40},
+    {
+        "Id": "27인치 이상",
+        "ConsumeWatt": 31},
+    {
+        "Id": "20인치 이상",
+        "ConsumeWatt": 24}
+    ] // TV
+
+var listitem_14 = [{
+        "Id": "콤팩트형",
+        "ConsumeWatt": 36},
+    {
+        "Id": "직관형",
+        "ConsumeWatt": 32}
+    ] // 형광램프
+
+  var listitem_16 = [{
+        "Id": "다리미",
+        "ConsumeWatt": 1000},
+    ] // 다리미
+  
+var listitem_17 = [{
+          "Id": "15kg",
+          "ConsumeWatt": 124.7},
+      {
+          "Id": "14kg",
+          "ConsumeWatt": 119.3},
+      {
+          "Id": "12kg",
+          "ConsumeWatt": 130.5},
+      {
+          "Id": "10kg",
+          "ConsumeWatt": 109.3}
+    ] // 세탁기 
+  
+var listitem_18 = [{
+        "Id": "슬림형",
+        "ConsumeWatt": 259.5},
+    {
+        "Id": "타워형",
+        "ConsumeWatt": 324.1},
+    {
+        "Id": "일체형/ 24인치 이상",
+        "ConsumeWatt": 105},
+    {
+        "Id": "일체형/ 23인치 이상",
+        "ConsumeWatt": 123.6},
+    {
+        "Id": "일체형/ 22인치 이상",
+        "ConsumeWatt": 60.8},
+    {
+        "Id": "일체형/ 21.5인치 이상",
+        "ConsumeWatt": 86},
+    ] // 컴퓨터
+
+var listitem_19 = [{
+        "Id": "65인치 이상",
+        "ConsumeWatt": 68},
+    {
+        "Id": "50인치 이상",
+        "ConsumeWatt": 57},
+    {
+        "Id": "42인치 이상",
+        "ConsumeWatt": 49},
+    {
+        "Id": "34인치 이상",
+        "ConsumeWatt": 40},
+    {
+        "Id": "27인치 이상",
+        "ConsumeWatt": 31},
+    {
+        "Id": "20인치 이상",
+        "ConsumeWatt": 24}
+    ] // 모니터 
+  
+var listitem_21 = [{
+        "Id": "일반",
+        "ConsumeWatt": 9}
+    ] // 충전기   
+  
+var listitem_22 = [{
+        "Id": "- 일반 -",
+        "ConsumeWatt": 0},
+    {
+        "Id": "15.6인치 이상",
+        "ConsumeWatt": 46.9},
+    {
+        "Id": "13인치 이상",
+        "ConsumeWatt": 42.1},
+    {
+        "Id": "11인치 이상",
+        "ConsumeWatt": 32},
+    {
+        "Id": "-----------------",
+        "ConsumeWatt": 0},
+    {
+        "Id": "- APPLE - ",
+        "ConsumeWatt": 0},
+    {
+        "Id": "PRO RETINA 15'",
+        "ConsumeWatt": 85},
+    {
+        "Id": "PRO RETINA 13'",
+        "ConsumeWatt": 60},
+    {
+        "Id": "PRO 15'",
+        "ConsumeWatt": 60},
+    {
+        "Id": "PRO 13'",
+        "ConsumeWatt": 60},
+    {
+        "Id": "Mac Book 13'",
+        "ConsumeWatt": 60},
+    {
+        "Id": "Air 11인치 이상'",
+        "ConsumeWatt": 45},
+    {
+        "Id": "Mac Book RETINA",
+        "ConsumeWatt": 29}
+    ] // 노트북 
+  
+var listitem_23 = [{
+        "Id": "셋톱박스",
+        "ConsumeWatt": 13.3}
+    ] // 셋톱박스 
+
+var listitem_24 = [{
+        "Id": "드라이기",
+        "ConsumeWatt": 700},
+    ] // 드라이기
+  
+var listitem_25 = [{
+        "Id": "- 스탠드형 -",
+        "ConsumeWatt": 0},
+    {
+        "Id": "20평 이상",
+        "ConsumeWatt": 414.5},
+    {
+        "Id": "15평 이상",
+        "ConsumeWatt": 319.4},
+    {
+        "Id": "- 벽걸이형 -",
+        "ConsumeWatt": 0},
+    {
+        "Id": "13평",
+        "ConsumeWatt": 281.9},
+    {
+        "Id": "8평 이상",
+        "ConsumeWatt": 207.3},
+    {
+        "Id": "6평 이상",
+        "ConsumeWatt": 152.5},
+    {
+        "Id": "- 천장형 -",
+        "ConsumeWatt": 0},
+    {
+        "Id": "18평",
+        "ConsumeWatt": 383.0},
+    {
+        "Id": "15평",
+        "ConsumeWatt": 297.2},
+    {
+        "Id": "13평",
+        "ConsumeWatt": 252.0}
+    ] // 에어컨  
+  
+var listitem_26 = [{
+        "Id": "스탠드형",
+        "ConsumeWatt": 42.8},
+    {
+        "Id": "벽걸이형",
+        "ConsumeWatt": 43.0}
+    ] // 선풍기
+
+var listitem_27 = [{
+        "Id": "스탠드형",
+        "ConsumeWatt": 2731},
+    {
+        "Id": "벽걸이형",
+        "ConsumeWatt": 2100},
+    {
+        "Id": "미니형",
+        "ConsumeWatt": 1203}
+    ] // 온풍기
+
+var listitem_28 = [{
+        "Id": "스탠드형",
+        "ConsumeWatt": 773.3},
+    {
+        "Id": "벽걸이형",
+        "ConsumeWatt": 1043.3},
+    {
+        "Id": "타워형",
+        "ConsumeWatt": 1150.0},
+    {
+        "Id": "미니형",
+        "ConsumeWatt": 800.0}
+    ] // 전열 난로
+  
+var listitem_29 = [{
+        "Id": "4인용",
+        "ConsumeWatt": 500.0},
+    {
+        "Id": "3인용",
+        "ConsumeWatt": 520.0},
+    {
+        "Id": "2인용",
+        "ConsumeWatt": 271.0},
+    {
+        "Id": "1인용",
+        "ConsumeWatt": 260.0}
+    ]  //온수매트 
+
+var listitem_30 = [{
+        "Id": "4인용",
+        "ConsumeWatt": 560.0},
+    {
+        "Id": "3인용",
+        "ConsumeWatt": 415.0},
+    {
+        "Id": "2인용",
+        "ConsumeWatt": 141.0},
+    {
+        "Id": "1인용",
+        "ConsumeWatt": 79.8}
+    ]  //전기장판 
+
+  var listitem_31 = [{
+          "Id": "19kg",
+          "ConsumeWatt": 640.4},
+      {
+          "Id": "17kg",
+          "ConsumeWatt": 552.3},
+      {
+          "Id": "16kg",
+          "ConsumeWatt": 574.2},
+      {
+          "Id": "15kg",
+          "ConsumeWatt": 600.8},
+      {
+          "Id": "13kg",
+          "ConsumeWatt": 473.1},
+      {
+          "Id": "12kg",
+          "ConsumeWatt": 560.6}
+    ] // 드럼세탁기   
+
+  
+  var listitems = {
+    "listitem1": listitem_1, 
+    "listitem4": listitem_4,
+    "listitem5": listitem_5,
+    "listitem10": listitem_10,
+    "listitem11": listitem_11,
+    "listitem13": listitem_13,
+    "listitem14": listitem_14,
+    "listitem16": listitem_16,
+    "listitem17": listitem_17,
+    "listitem18": listitem_18,
+    "listitem19": listitem_19,
+    "listitem21": listitem_21,
+    "listitem22": listitem_22,
+    "listitem23": listitem_23,
+    "listitem24": listitem_24,
+    "listitem25": listitem_25,
+    "listitem26": listitem_26,
+    "listitem27": listitem_27,
+    "listitem28": listitem_28,
+    "listitem29": listitem_29,
+    "listitem30": listitem_30,
+    "listitem31": listitem_31
+
+  }
+
+
+//주택용 고압
+//기본 요금
+var usetype_higher_base = [{
+        "Id": "base_lebel1",
+        "ConsumeFee": 410},
+    {
+        "Id": "base_lebel2",
+        "ConsumeFee": 730},
+    {
+        "Id": "base_lebel3",
+        "ConsumeFee": 1260},
+    {
+        "Id": "base_lebel4",
+        "ConsumeFee": 3170},
+    {
+        "Id": "base_lebel5",
+        "ConsumeFee": 6060},
+    {
+        "Id": "base_lebel6",
+        "ConsumeFee": 10760}
+    ]
+
+//주택용 고압
+//전력량 요금
+var usetype_higher_electric_fee = [{
+        "Id": "elec_level1",
+        "ConsumeFee": 57.6 },
+    {
+        "Id": "elec_level2",
+        "ConsumeFee": 98.9},
+    {
+        "Id": "elec_level3",
+        "ConsumeFee": 147.3},
+    {
+        "Id": "elec_level4",
+        "ConsumeFee": 215.6},
+    {
+        "Id": "elec_level5",
+        "ConsumeFee": 325.7},
+    {
+        "Id": "elec_level6",
+        "ConsumeFee": 574.6}
+    ]
+
+//주택용 저압
+//기본 요금
+var usetype_lower_base = [{
+        "Id": "base_lebel1",
+        "ConsumeFee": 410},
+    {
+        "Id": "base_lebel2",
+        "ConsumeFee": 910},
+    {
+        "Id": "base_lebel3",
+        "ConsumeFee": 1600},
+    {
+        "Id": "base_lebel4",
+        "ConsumeFee": 3850},
+    {
+        "Id": "base_lebel5",
+        "ConsumeFee": 7300},
+    {
+        "Id": "base_lebel6",
+        "ConsumeFee": 12940}
+    ]
+
+//주택용 저압
+//전력량 요금
+var usetype_lower_electric_fee = [{
+        "Id": "elec_level1",
+        "ConsumeFee": 60.7},
+    {
+        "Id": "elec_level2",
+        "ConsumeFee": 125.9},
+    {
+        "Id": "elec_level3",
+        "ConsumeFee": 187.9},
+    {
+        "Id": "elec_level4",
+        "ConsumeFee": 280.6},
+    {
+        "Id": "elec_level5",
+        "ConsumeFee": 417.7},
+    {
+        "Id": "elec_level6",
+        "ConsumeFee": 709.9}
+    ];
+
+//주택용 고압
+//기본 요금
+var usetype_higher_base = [{
+        "Id": "base_lebel1",
+        "ConsumeFee": 410},
+    {
+        "Id": "base_lebel2",
+        "ConsumeFee": 730},
+    {
+        "Id": "base_lebel3",
+        "ConsumeFee": 1260},
+    {
+        "Id": "base_lebel4",
+        "ConsumeFee": 3170},
+    {
+        "Id": "base_lebel5",
+        "ConsumeFee": 6060},
+    {
+        "Id": "base_lebel6",
+        "ConsumeFee": 10760}
+    ]
+
+//주택용 고압
+//전력량 요금
+var usetype_higher_electric_fee = [{
+        "Id": "elec_level1",
+        "ConsumeFee": 57.6 },
+    {
+        "Id": "elec_level2",
+        "ConsumeFee": 98.9},
+    {
+        "Id": "elec_level3",
+        "ConsumeFee": 147.3},
+    {
+        "Id": "elec_level4",
+        "ConsumeFee": 215.6},
+    {
+        "Id": "elec_level5",
+        "ConsumeFee": 325.7},
+    {
+        "Id": "elec_level6",
+        "ConsumeFee": 574.6}
+    ]
+
+//주택용 저압
+//기본 요금
+var usetype_lower_base = [{
+        "Id": "base_lebel1",
+        "ConsumeFee": 410},
+    {
+        "Id": "base_lebel2",
+        "ConsumeFee": 910},
+    {
+        "Id": "base_lebel3",
+        "ConsumeFee": 1600},
+    {
+        "Id": "base_lebel4",
+        "ConsumeFee": 3850},
+    {
+        "Id": "base_lebel5",
+        "ConsumeFee": 7300},
+    {
+        "Id": "base_lebel6",
+        "ConsumeFee": 12940}
+    ]
+
+//주택용 저압
+//전력량 요금
+var usetype_lower_electric_fee = [{
+        "Id": "elec_level1",
+        "ConsumeFee": 60.7},
+    {
+        "Id": "elec_level2",
+        "ConsumeFee": 125.9},
+    {
+        "Id": "elec_level3",
+        "ConsumeFee": 187.9},
+    {
+        "Id": "elec_level4",
+        "ConsumeFee": 280.6},
+    {
+        "Id": "elec_level5",
+        "ConsumeFee": 417.7},
+    {
+        "Id": "elec_level6",
+        "ConsumeFee": 709.9}
+    ];
+
+//데이터셋 라벨
+//usetype_lower_base
+//usetype_lower_electric_fee
+var usetypes = {
+"usetype_lower_base" : usetype_lower_base,
+"usetype_higher_base" : usetype_higher_base,
+"usetype_lower_electric_fee" : usetype_lower_electric_fee,
+"usetype_higher_electric_fee" : usetype_higher_electric_fee
+}
